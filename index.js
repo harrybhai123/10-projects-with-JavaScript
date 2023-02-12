666c15e6559fbf98786e5305b369bbc74860c401
Hariom Tomar <68967590+harrybhai123@users.noreply.github.com>
2023-02-12 17:26:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,15 +16,40 @@ function timeSection() {
 let timeOfTheDay
 let hours = date.getHours()
 
-if (hours < 12 && hours > 6) { 
-    timeOfTheDay = "Hello Dear, Good Morning 🌄"
-} else if (hours > 12 && hours < 17) {
-    timeOfTheDay = "Hello Dear, Good AfterNoon 🕑"
-}else if (hours > 17 && hours <  20) {
-    timeOfTheDay = "Hello Dear, Good Evening 🌆"
-} else {
-    timeOfTheDay = "Hello Dear, Good Night 🌃"
-    }
+const greetEl = document.getElementById("greet");
+const timeEl = document.getElementById("time");
+const dateEl = document.getElementById("date");
+
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data.urls.full);
+    document.body.style.backgroundImage = `url(${data.urls.full})`;
+  });
+
+function timeSection() {
+  let date = new Date();
+  let d = date.toLocaleDateString("en-in");
+  let t = date.toLocaleTimeString("en-in");
+  let hours = date.getHours();
+  let timeOfDay;
+
+  if (hours < 12 && hours > 6) {
+    timeOfDay = "Hello Dear, Good Morning 🌄";
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "Hello Dear, Good Afternoon 🕑";
+  } else if (hours >= 17) {
+    timeOfDay = "Hello Dear, Good Evening 🌃";
+  } else if (hours >= 20) {
+    timeOfDay = "Hello Dear, Good Night 😴";
+  }
+  dateEl.textContent = d;
+  timeEl.textContent = t;
+  greetEl.innerText = timeOfDay;
+}
+
+setInterval(timeSection, 1000);
+
     
 
 let d = date.toLocaleDateString("en-in")
